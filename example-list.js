25ebfd5e1e0858e7875081629a55c66c5e46e42b
Abran <70737273+Abran7411@users.noreply.github.com>
2021-11-25 18:54:48 +0530

    Update example-list.js

--- example-list.js
+++ example-list.js
@@ -3,13 +3,13 @@ import HelloWorld1 from "./examples/1-hello-world/1.HelloWorld1";
 import HelloWorld2 from "./examples/1-hello-world/2.HelloWorld2";
 import MomoLogin from "./examples/2-login-screen/1.MomoLogin";
 import FacebookLogin from "./examples/2-login-screen/2.FacebookLogin";
-import TheLight from "./examples/3-the-light/1.TheLight";
-import TrafficLight from "./examples/3-the-light/2.TrafficLight";
+// import TheLight from "./examples/3-the-light/1.TheLight";
+// import TrafficLight from "./examples/3-the-light/2.TrafficLight";
 import RegisterForm from "./examples/4-register-form/RegisterForm";
 import InstagramFeed from "./examples/5-instagram-feed/InstagramFeed";
-import RockPaperScissors from "./examples/6-rock-paper-scissors/RockPaperScissors";
-import ScanQrCode from "./examples/7-scan-qr-code/ScanQrCode";
-import StopWatch from "./examples/8-stopwatch/StopWatch";
+// import RockPaperScissors from "./examples/6-rock-paper-scissors/RockPaperScissors";
+// import ScanQrCode from "./examples/7-scan-qr-code/ScanQrCode";
+// import StopWatch from "./examples/8-stopwatch/StopWatch";
 import BMICalculator from "./examples/9-bmi-calculator/BMICalculator";
 import MusicPlayer from "./examples/10-music-player/MusicPlayer";
 import WorldwideNews from "./examples/11-news/WorldwideNews";
@@ -36,15 +36,15 @@ export const EXAMPLE_LIST = [
     level: 2,
     component: <FacebookLogin />,
   },
-  {
-    name: "The Light",
-    level: 3,
-    component: <TheLight />,
-  },
-  {
-    name: "Traffic Light",
-    level: 3,
-    component: <TrafficLight />,
+//   {
+//     name: "The Light",
+//     level: 3,
+//     component: <TheLight />,
+//   },
+//   {
+//     name: "Traffic Light",
+//     level: 3,
+//     component: <TrafficLight />,
   },
   {
     name: "Register Form",
@@ -56,21 +56,21 @@ export const EXAMPLE_LIST = [
     level: 5,
     component: <InstagramFeed />,
   },
-  {
-    name: "Rock Paper Scissors",
-    level: 6,
-    component: <RockPaperScissors />,
-  },
-  {
-    name: "Scan QR Code",
-    level: 7,
-    component: <ScanQrCode />,
-  },
-  {
-    name: "Stop Watch",
-    level: 8,
-    component: <StopWatch />,
-  },
+//   {
+//     name: "Rock Paper Scissors",
+//     level: 6,
+//     component: <RockPaperScissors />,
+//   },
+//   {
+//     name: "Scan QR Code",
+//     level: 7,
+//     component: <ScanQrCode />,
+//   },
+//   {
+//     name: "Stop Watch",
+//     level: 8,
+//     component: <StopWatch />,
+//   },
   {
     name: "BMI Calculator",
     level: 9,
