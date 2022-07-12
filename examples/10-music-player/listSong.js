24395e5de6666ea5884e714d1ae0631d67275101
Abran7411 <abdoss7411@gmail.com>
2022-07-12 15:31:27 +0530

    master

---
+++ examples/10-music-player/listSong.js
@@ -0,0 +1,38 @@
+export const PLAY_LIST = [
+  {
+    sourceUri:
+      "https://github.com/robinhuy/robinhuy/blob/master/musics/hoa-hai-duong.mp3?raw=true",
+    name: "Soul of Doctor",
+    coverImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQJJfuldeIIftNTjivKFPPYy8OXAwteoLMPQ&usqp=CAU",
+    singer: "Anirudh R",
+    duration: 229632,
+  },
+  {
+    sourceUri:
+      "https://github.com/robinhuy/robinhuy/blob/master/musics/ai-mang-co-don-di.mp3?raw=true",
+    name: "Billa",
+    coverImage:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b6/Yuvan_Shankar_Raja_exclusive_HQ_Photos_Silverscreen.jpg",
+    singer: "Yuvan Shankar Raja",
+    duration: 221231,
+  },
+  {
+    sourceUri:
+      "https://github.com/robinhuy/robinhuy/blob/master/musics/bong-hoa-dep-nhat.mp3?raw=true",
+    name: "U1 Background-Track",
+    coverImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAlERSwdRDlk54t4pbP-4Nu9YaqXxAuzESaQ&usqp=CAU",
+    singer: "Yuvan Shankar Raja",
+    duration: 315312,
+  },
+  {
+    sourceUri:
+      "https://github.com/robinhuy/robinhuy/blob/master/musics/thien-dang.mp3?raw=true",
+    name: "AA Background Track",
+    coverImage:
+      "https://user-images.githubusercontent.com/12640832/100080888-f10c4b00-2e78-11eb-931c-74272c77cad4.jpg",
+    singer: "S.Thaman",
+    duration: 232200,
+  },
+];
---
+++ examples/5-instagram-feed/data.js
@@ -0,0 +1,71 @@
+export const profile = {
+    avatar: require("../../assets/instagram-feed/avatar2.jpg"),
+};
+
+export const stories = [{
+        id: 1,
+        avatar: require("../../assets/instagram-feed/avatar2.jpg"),
+        name: "@rahulVijay",
+        isSeen: false,
+    },
+    {
+        id: 2,
+        avatar: require("../../assets/instagram-feed/avatar2.jpg"),
+        name: "ramesh67",
+        isSeen: false,
+    },
+    {
+        id: 3,
+        avatar: require("../../assets/instagram-feed/avatar2.jpg"),
+        name: "a*k",
+        isSeen: true,
+    },
+    {
+        id: 4,
+        avatar: require("../../assets/instagram-feed/avatar4.jpg"),
+        name: "gopi_official",
+        isSeen: true,
+    },
+];
+
+export const articles = [{
+        id: 1,
+        avatar: require("../../assets/instagram-feed/avatar2.jpg"),
+        name: "@rahulVijay",
+        image: require("../../assets/instagram-feed/img1.jpg"),
+        likeCount: "203",
+        date: "1 day ago",
+    },
+    {
+        id: 2,
+        avatar: require("../../assets/instagram-feed/avatar2.jpg"),
+        name: "@rahulVijay",
+        image: require("../../assets/instagram-feed/img2.jpg"),
+        likeCount: "406",
+        date: "3 days ago",
+    },
+    {
+        id: 3,
+        avatar: require("../../assets/instagram-feed/avatar2.jpg"),
+        name: "a*k",
+        image: require("../../assets/instagram-feed/img4.jpg"),
+        likeCount: "7002",
+        date: "8 days ago",
+    },
+    {
+        id: 4,
+        avatar: require("../../assets/instagram-feed/avatar2.jpg"),
+        name: "ramesh67",
+        image: require("../../assets/instagram-feed/img4.jpg"),
+        likeCount: "19",
+        date: "1 year ago",
+    },
+    {
+        id: 5,
+        avatar: require("../../assets/instagram-feed/avatar2.jpg"),
+        name: "gopi_official",
+        image: require("../../assets/instagram-feed/img5.jpg"),
+        likeCount: "12",
+        date: "2 years ago",
+    },
+];
